b60dc46b1e325fdf086982164e0db2b07cc75e3e
celpratter <celina.pratter@htl-salzburg.ac.at>
2019-12-10 16:42:38 +0100

    added comments

--- Assets/MyGame/Scripts/ComputeNumbers.cs
+++ Assets/MyGame/Scripts/ComputeNumbers.cs
@@ -3,15 +3,18 @@ using UnityEngine.UI;
 
 public class ComputeNumbers : MonoBehaviour
 {
+    // init variables
     public InputField ipVarA;
     public InputField ipVarB;
     public Text result;
+    //show result in text box
     public void SetResult()
     {
         float a = float.Parse(ipVarA.text);
         float b = float.Parse(ipVarB.text);
         result.text = SubstractNumbers(a, b).ToString();
     }
+    // substract numbers, return result value
     private float SubstractNumbers(float variableA, float variableB)
     {
         float result = variableA - variableB;
