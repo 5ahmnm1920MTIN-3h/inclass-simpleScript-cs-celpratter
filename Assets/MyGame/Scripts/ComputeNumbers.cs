f3669e9aa1221023ef6442cd745806e6869dc41a
celpratter <celina.pratter@htl-salzburg.ac.at>
2019-12-10 16:36:41 +0100

    delete unnecessary spaces

--- Assets/MyGame/Scripts/ComputeNumbers.cs
+++ Assets/MyGame/Scripts/ComputeNumbers.cs
@@ -8,18 +8,15 @@ public class ComputeNumbers : MonoBehaviour
     public InputField ipVarA;
     public InputField ipVarB;
     public Text result;
-
     public void SetResult()
     {
         float a = float.Parse(ipVarA.text);
         float b = float.Parse(ipVarB.text);
         result.text = SubstractNumbers(a, b).ToString();
     }
-
     private float SubstractNumbers(float variableA, float variableB)
     {
         float result = variableA - variableB;
         return result;
     }
-
 }
